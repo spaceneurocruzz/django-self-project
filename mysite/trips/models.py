5f8debd5c8e49ecde3078f36a19563291b8ccb4c
Sherlock Holmes <s1127925@gmail.com>
2016-01-31 00:59:12 +0800

    Initial commit

---
+++ mysite/trips/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    photo = models.URLField(blank=True)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
